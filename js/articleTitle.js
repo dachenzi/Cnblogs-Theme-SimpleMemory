fb5d4718d041f3b7959ecddd6d9fb63b00917131
dahl <beyondlee2011@126.com>
2019-03-30 10:49:56 +0800

    update articletitle.js

--- js/articleTitle.js
+++ js/articleTitle.js
@@ -9,7 +9,8 @@ $(document).ready(function () {
     }
     ch1 = s.find('h1');
     ch2 = s.find('h2');
-
+    ch3 = s.find('h3');
+    ch4 = s.find('h4');
     if (ch1.length > 0) {
         for (var i = 0; i < ch1.length; i++) {
             var th1 = $(ch1[i]);
@@ -70,4 +71,118 @@ $(document).ready(function () {
             ) ;
         }
     }
-});
+
+    
+    
+        if (ch2.length > 0) {
+        var num  = 0;
+        var last = 0;
+        for (i = 0; i < ch2.length; i++) {
+            var th2 = $(ch2[i]);
+            th2.wrap('<span title-type="h2" class="header__span"></span>');
+            var th2Text = th2.text();
+            th2.text('');
+            th2.addClass('header__dev');
+            var their = th2.parents('.header__span').prevAll('.header__span[title-type="h1"]');
+            if (their.length > 0) {
+                their = $(their[0]);
+                var current  = their.find('.dev__fe i').text();
+                if (current != last) {
+                    num  = 0;
+                }
+                last = current;
+            } else {
+                num = 0;
+            }
+            var th2Html = '<b class="dev__fe"><i>'+last+'</i></b>';
+            th2Html += '<span class="dev__slash">|</span>';
+            th2Html += '<b class="dev__ux"><i>'+(++num)+'</i></b>';
+            th2Html += '<b class="dev__developer"><span class="dev__title">'+th2Text+'</span></b>';
+            th2.append(th2Html);
+            th2.parents('.header__span').after('<br>');
+            th2.parent(".header__span").hover(
+                function(){
+                    $(this).find('.header__dev').addClass("header__dev--open");
+                } ,
+                function(){
+                    $(this).find('.header__dev').removeClass("header__dev--open");
+                }
+            ) ;
+        }
+    }
+    if (ch3.length > 0) {
+    var num  = 0;
+    var last = 0;
+    for (i = 0; i < ch3.length; i++) {
+        var th3 = $(ch3[i]);
+        th3.wrap('<span title-type="h3" class="header__span"></span>');
+        var th3Text = th3.text();
+        th3.text('');
+        th3.addClass('header__dev');
+        var their = th3.parents('.header__span').prevAll('.header__span[title-type="h2"]');
+        if (their.length > 0) {
+            their = $(their[0]);
+            var current  = their.find('.dev__fe i').text();
+            if (current != last) {
+                num  = 0;
+            }
+            last = current;
+        } else {
+            num = 0;
+        }
+        var th3Html = '<b class="dev__fe"><i>'+last+'</i></b>';
+        th3Html += '<span class="dev__slash">|</span>';
+        th3Html += '<b class="dev__ux"><i>'+(++num)+'</i></b>';
+        th3Html += '<b class="dev__developer"><span class="dev__title">'+th3Text+'</span></b>';
+        th3.append(th2Html);
+        th3.parents('.header__span').after('<br>');
+        th3.parent(".header__span").hover(
+            function(){
+                $(this).find('.header__dev').addClass("header__dev--open");
+            } ,
+            function(){
+                $(this).find('.header__dev').removeClass("header__dev--open");
+            }
+        ) ;
+    }
+}
+
+    if (ch4.length > 0) {
+        var num  = 0;
+        var last = 0;
+        for (i = 0; i < ch4.length; i++) {
+            var th4 = $(ch4[i]);
+            th2.wrap('<span title-type="h4" class="header__span"></span>');
+            var th4Text = th4.text();
+            th4.text('');
+            th4.addClass('header__dev');
+            var their = th4.parents('.header__span').prevAll('.header__span[title-type="h3"]');
+            if (their.length > 0) {
+                their = $(their[0]);
+                var current  = their.find('.dev__fe i').text();
+                if (current != last) {
+                    num  = 0;
+                }
+                last = current;
+            } else {
+                num = 0;
+            }
+            var th4Html = '<b class="dev__fe"><i>'+last+'</i></b>';
+            th4Html += '<span class="dev__slash">|</span>';
+            th4Html += '<b class="dev__ux"><i>'+(++num)+'</i></b>';
+            th4Html += '<b class="dev__developer"><span class="dev__title">'+th4Text+'</span></b>';
+            th4.append(th2Html);
+            th4.parents('.header__span').after('<br>');
+            th4.parent(".header__span").hover(
+                function(){
+                    $(this).find('.header__dev').addClass("header__dev--open");
+                } ,
+                function(){
+                    $(this).find('.header__dev').removeClass("header__dev--open");
+                }
+            ) ;
+        }
+    }
+
+
+    });
